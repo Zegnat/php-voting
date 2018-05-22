4be8b8fd12f17c82ff6ef2349d948f79ddbb5b56
Martijn van der Ven <martijn@vanderven.se>
2018-05-22 18:00:08 +0200

    Expand IRV tests with variations and simulations

--- tests/Ranked/IRVTest.php
+++ tests/Ranked/IRVTest.php
@@ -21,17 +21,108 @@ final class IRVTest extends TestCase
         new IRV(new Set());
     }
 
-    public function testCanBeCreatedWithCandidate(): IRV
+    public function testCanBeCreatedWithCandidates(): void
     {
-        $instance = new IRV(new Set(['Alice']));
-        $this->assertInstanceOf(IRV::class, $instance);
-        return $instance;
+        $this->assertInstanceOf(IRV::class, new IRV(new Set(['Alice', 'Bob', 'Carol'])));
     }
 
-    public function testCanBeCreatedWithMultipleCandidates(): IRV
+    public function testFullPreferentialVotingIgnoresIncompleteBallots(): void
     {
-        $instance = new IRV(new Set(['Alice', 'Bob']));
-        $this->assertInstanceOf(IRV::class, $instance);
-        return $instance;
+        $election = new IRV(new Set(['Alice', 'Bob', 'Carol']), IRV::FULL);
+        $new = $election->cast(new Set(['Carol', 'Bob']));
+        $this->assertSame($election, $new);
+    }
+
+    public function testFullPreferentialVotingAcceptsCompleteBallots(): void
+    {
+        $election = new IRV(new Set(['Alice', 'Bob', 'Carol']), IRV::FULL);
+        $new = $election->cast(new Set(['Carol', 'Bob', 'Alice']));
+        $this->assertNotSame($election, $new);
+    }
+
+    public function testFullPreferentialVotingAcceptsCompleteBallotsWithExtras(): void
+    {
+        $election = new IRV(new Set(['Alice', 'Bob', 'Carol']), IRV::FULL);
+        $new = $election->cast(new Set(['Carol', 'Bob', 'Alice', 'None']));
+        $this->assertNotSame($election, $new);
+    }
+
+    public function testStrictFullPreferentialVotingIgnoresCompleteBallotsWithExtras(): void
+    {
+        $election = new IRV(new Set(['Alice', 'Bob', 'Carol']), IRV::FULL | IRV::STRICT);
+        $new = $election->cast(new Set(['Carol', 'Bob', 'Alice', 'None']));
+        $this->assertSame($election, $new);
+    }
+
+    public function testOptionalPreferentialVotingAcceptsIncompleteBallots(): void
+    {
+        $election = new IRV(new Set(['Alice', 'Bob', 'Carol']), IRV::OPTIONAL);
+        $new = $election->cast(new Set(['Carol', 'Bob']));
+        $this->assertNotSame($election, $new);
+    }
+
+    public function testOptionalPreferentialVotingAcceptsCompleteBallots(): void
+    {
+        $election = new IRV(new Set(['Alice', 'Bob', 'Carol']), IRV::OPTIONAL);
+        $new = $election->cast(new Set(['Carol', 'Bob', 'Alice']));
+        $this->assertNotSame($election, $new);
+    }
+
+    public function testOptionalPreferentialVotingAcceptsBallotsWithExtras(): void
+    {
+        $election = new IRV(new Set(['Alice', 'Bob', 'Carol']), IRV::OPTIONAL);
+        $new = $election->cast(new Set(['Carol', 'Bob', 'Alice', 'None']));
+        $this->assertNotSame($election, $new);
+    }
+
+    public function testStrictOptionalPreferentialVotingIgnoresBallotsWithExtras(): void
+    {
+        $election = new IRV(new Set(['Alice', 'Bob', 'Carol']), IRV::OPTIONAL | IRV::STRICT);
+        $new = $election->cast(new Set(['Carol', 'Bob', 'None']));
+        $this->assertSame($election, $new);
+    }
+
+    /**
+     * @depends testCanBeCreatedWithCandidates
+     * @depends testFullPreferentialVotingAcceptsCompleteBallots
+     * @see https://en.wikipedia.org/wiki/Instant-runoff_voting#Five_voters,_three_candidates
+     */
+    public function testSimulateFiveVotersThreeCandidates(): void
+    {
+        $election = new IRV(new Set(['Bob', 'Sue', 'Bill']));
+        $votes = [
+            'a' => ['Bob', 'Bill', 'Sue'],
+            'b' => ['Sue', 'Bob', 'Bill'],
+            'c' => ['Bill', 'Sue', 'Bob'],
+            'd' => ['Bob', 'Bill', 'Sue'],
+            'e' => ['Sue', 'Bob', 'Bill'],
+        ];
+        foreach ($votes as $ballot) {
+            $election = $election->cast(new Set($ballot));
+        }
+        $this->assertSame(['Sue'], $election->getWinner()->toArray());
+    }
+
+    /**
+     * @depends testCanBeCreatedWithCandidates
+     * @depends testFullPreferentialVotingAcceptsCompleteBallots
+     * @see https://en.wikipedia.org/wiki/Instant-runoff_voting#Tennessee_capital_election
+     */
+    public function testSimulateTennesseeCapitalElection(): void
+    {
+        $election = new IRV(new Set(['Memphis', 'Nashville', 'Knoxville', 'Chattanooga']));
+        $votes = [
+            42 => ['Memphis', 'Nashville', 'Chattanooga', 'Knoxville'],
+            26 => ['Nashville', 'Chattanooga', 'Knoxville', 'Memphis'],
+            15 => ['Chattanooga', 'Knoxville', 'Nashville', 'Memphis'],
+            17 => ['Knoxville', 'Chattanooga', 'Nashville', 'Memphis'],
+        ];
+        foreach ($votes as $times => $ballot) {
+            while ($times > 0) {
+                $election = $election->cast(new Set($ballot));
+                $times -= 1;
+            }
+        }
+        $this->assertSame(['Knoxville'], $election->getWinner()->toArray());
     }
 }
